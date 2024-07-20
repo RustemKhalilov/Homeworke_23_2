c6ad7fbe761a2792246dfcc40f57e059d024b808
Rustem Khalilov <rvkhalilov@gmail.com>
2024-07-20 18:00:40 +0500

    Homeworke 23.2 logout modif

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
    path("login/", LoginView.as_view(template_name='login.html'), name='login'),
-   path("logout/", LogoutView.as_view(), name='logout'),
+   path("logout/", LogoutView.as_view(next_page='/'), name='logout'),
    path("register/", UserCreateView.as_view(), name='register'),
    path('password_recovery/', PasswordRecoveryView.as_view(), name='password_recovery'),
    path("email-confirm/<str:token>/", email_verification, name='email-confirm'),
